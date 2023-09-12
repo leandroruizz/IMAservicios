bf2ec886f0ed421be54119146905432f1fc7ab74
lleandroruizz <leandrolmedo2000@gmail.com>
2023-09-12 13:22:21 -0300

    form y boton wsp funcional

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,5 @@
 import './contact.css'
+import Link from 'next/link'
 
 export default function Contact() {
     return (
@@ -7,23 +8,23 @@ export default function Contact() {
               <h1 className='titulo-contacto'>Contáctenos</h1>
               <p className='texto-contacto'>Eco Industrial está listo para colaborar con su organización y generar beneficios económicos reales a través de soluciones sostenibles y mantenimiento integral de sus instalaciones. ¡Contáctenos hoy mismo para comenzar a construir un futuro más eficiente y rentable!</p>
               <div className='div-button'>
-                 <button className='whatsapp'>Whatsapp</button>
+                 <Link className='whatsapp' href='https://api.whatsapp.com/send?phone=543512330090' target='_blank'>Whatsapp</Link>
               </div>
-              <form method="post" className='formulario'>
+              <form action="https://formsubmit.co/leandrolmedo2000@gmail.com" method="post" className='formulario'>
                  <label>
                      <h3 className='cont-contacto'>Nombre</h3>
-                     <input type="text" name="nombre" />
+                     <input type="nombre" name="nombre" required="true" />
                  </label>
                  <label>
                      <h3 className='cont-contacto'>E-Mail</h3>
-                     <input type="email" name="email" />
+                     <input type="email" name="email" required="true" />
                  </label>
                  <label>
-                     <h3 className='cont-contacto'>Consulta</h3>
-                     <textarea name="comentario"></textarea>
+                     <h3 className='cont-contacto'>Mensaje</h3>
+                     <textarea className='textarea' name="mensaje" required="true"></textarea>
                  </label>
-                 <input type="hidden" name="form-name" value="contacto" />
-                 <button type="submit">Enviar</button>
+                 <button className='boton-enviar'type="submit">Enviar</button>
+                 <input type="hidden" name="_next" value="http://localhost:3000/" />
               </form>
             </div>
         </main>
